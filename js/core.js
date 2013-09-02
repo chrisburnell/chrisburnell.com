525297b3d6a0f905deb045a642a797ffe2ac8344
Chris Burnell <me@chrisburnell.com>
2013-09-02 10:42:30 +0100

    RSS link in footer
    Signed-off-by: Chris Burnell <me@chrisburnell.com>

--- js/core.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( function() {
-
-    // Forces external links to open in a new tab without breaking validation
-    $("a[href*='http://']:not([href*='"+location.hostname+"'])").attr("target","_blank");
-
-    // Article Tags - remove the <strong> tags around the 'and'
-    $('.article-tags').html( $('.article-tags').html().replace('and','</strong>and<strong>') );
-
-});
