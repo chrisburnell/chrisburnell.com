e87040408995824e253923699bae261594745b1d
Chris Burnell <me@chrisburnell.com>
2014-01-22 09:39:51 +0000

    gulp!

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Gulp Configuration
+
+var gulp = require('gulp');
+    sass = require('gulp-sass'),
+    minifycss = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify');
+
+gulp.task('styles', function() {
+    return gulp.src('css/ravenous.scss')
+        .pipe(sass({ style: 'expanded', includePaths: ['css/'] }))
+        .pipe(gulp.dest('css/'))
+        .pipe(rename('ravenous.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css/'))
+        .pipe(notify({ title: 'Gulp', message: 'SASS compiled.', onLast: true,  }));
+});
+
+gulp.task('default', function() {
+    gulp.run('styles');
+});
+
+gulp.task('watch', function() {
+    gulp.watch('css/**/*.scss', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', compiling...');
+        gulp.run('styles');
+    });
+});
