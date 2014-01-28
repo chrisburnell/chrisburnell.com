8cfe28207501bc40cc5d25d97f03c9dc215d5600
Chris Burnell <me@chrisburnell.com>
2014-01-28 11:50:04 +0000

    Proper gulp! + comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-// Gulp Configuration
+/**
+ * Gulp Configuration
+ */
 
 var gulp = require('gulp');
     sass = require('gulp-sass'),
@@ -8,24 +10,42 @@ var gulp = require('gulp');
     notify = require('gulp-notify'),
     autoprefix = require('gulp-autoprefixer');
 
-gulp.task('styles', function() {
-    return gulp.src('css/ravenous.scss')
-        .pipe(sass({ style: 'expanded', includePaths: ['css/'] }))
+var cssDir = 'css/',
+    imagesDir = 'images/';
+
+// Compile SCSS, autoprefix CSS3, and minify
+gulp.task('css', function() {
+    return gulp.src(cssDir + 'ravenous.scss')
+        .pipe(sass({ style: 'expanded', includePaths: [cssDir] }))
         .pipe(autoprefix("last 2 versions", "> 1%"))
-        .pipe(gulp.dest('css/'))
+        .pipe(gulp.dest(cssDir))
         .pipe(rename('ravenous.min.css'))
         .pipe(minifycss())
-        .pipe(gulp.dest('css/'))
-        .pipe(notify({ title: 'Gulp', message: 'SASS compiled.', onLast: true }));
+        .pipe(gulp.dest(cssDir))
+        .pipe(notify({ title: 'Gulp', message: 'CSS compiled.', onLast: true }));
 });
 
+// Crush images
+gulp.task('images', function() {
+    return gulp.src('images/**/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('images'))
+        .pipe(notify({ title: 'Gulp', message: 'Images crushed.', onLast: true }));
+});
+
+// Default task
 gulp.task('default', function() {
-    gulp.run('styles');
+    gulp.run('css');
 });
 
+// Watch files and perform tasks
 gulp.task('watch', function() {
-    gulp.watch('css/**/*.scss', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', compiling...');
-        gulp.run('styles');
+    gulp.watch(cssDir + '**/*.scss', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('css');
+    });
+    gulp.watch(imagesDir + '**/*', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('images');
     });
 });
