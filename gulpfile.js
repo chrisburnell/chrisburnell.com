0aa51cc9f3910e702e7a21d8bd80f498be0533d3
Chris Burnell <me@chrisburnell.com>
2014-01-28 12:14:30 +0000

    gulpfile update + livereload + package.json update

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,19 @@ var gulp = require('gulp');
     sass = require('gulp-sass'),
     minifycss = require('gulp-minify-css'),
     imagemin = require('gulp-imagemin'),
+    svgmin = require('gulp-svgmin'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
-    autoprefix = require('gulp-autoprefixer');
+    autoprefix = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    server = lr();
 
 var cssDir = 'css/',
     imagesDir = 'images/';
 
 // Compile SCSS, autoprefix CSS3, and minify
-gulp.task('css', function() {
+gulp.task('styles', function() {
     return gulp.src(cssDir + 'ravenous.scss')
         .pipe(sass({ style: 'expanded', includePaths: [cssDir] }))
         .pipe(autoprefix("last 2 versions", "> 1%"))
@@ -22,30 +26,52 @@ gulp.task('css', function() {
         .pipe(rename('ravenous.min.css'))
         .pipe(minifycss())
         .pipe(gulp.dest(cssDir))
+        .pipe(livereload(server))
         .pipe(notify({ title: 'Gulp', message: 'CSS compiled.', onLast: true }));
 });
 
 // Crush images
 gulp.task('images', function() {
-    return gulp.src('images/**/*')
+    return gulp.src(imagesDir + '**/*')
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('images'))
+        .pipe(gulp.dest(imagesDir))
+        .pipe(livereload(server))
         .pipe(notify({ title: 'Gulp', message: 'Images crushed.', onLast: true }));
+});
+
+// Crush SVGs
+gulp.task('svg', function() {
+    return gulp.src(imagesDir + '**/*')
+        .pipe(svgmin())
+        .pipe(gulp.dest(imagesDir))
+        .pipe(livereload(server))
+        .pipe(notify({ title: 'Gulp', message: 'SVGs crushed.', onLast: true }));
 });
 
 // Default task
 gulp.task('default', function() {
-    gulp.run('css');
+    gulp.run('styles');
 });
 
 // Watch files and perform tasks
 gulp.task('watch', function() {
-    gulp.watch(cssDir + '**/*.scss', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        gulp.run('css');
+
+    // Listen on port 35729
+    server.listen(35729, function(err) {
+        if (err) {
+            return console.log(err)
+        };
+        // Watch CSS files
+        gulp.watch(cssDir + '**/*.scss', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            gulp.run('styles');
+        });
+        // Watch images and SVGs
+        gulp.watch(imagesDir + '**/*', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            gulp.run('images');
+            gulp.run('svg');
+        });
     });
-    gulp.watch(imagesDir + '**/*', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        gulp.run('images');
-    });
+
 });
