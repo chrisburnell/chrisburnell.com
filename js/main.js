ff42920d76fefa06dc60cdab8585338e5e6bcc19
Chris Burnell <me@chrisburnell.com>
2015-11-16 14:24:03 +0000

    disqus comment count fix

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@
  */
 
 (function () {
-    var dsqc = document.createElement('script'); dsqc.type = 'text/javascript'; dsqc.async = true;
+    var dsqc = document.createElement('script'); dsqc.type = 'text/javascript'; dsqc.id = 'dsq-count-scr'; dsqc.async = true;
     dsqc.src = '//' + DISQUS_SHORTNAME + '.disqus.com/count.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsqc);
 }());
