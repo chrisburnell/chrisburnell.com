7a9f9092a410e8552071785516144e02aaf1a2f0
Chris Burnell <me@chrisburnell.com>
2016-04-12 15:33:48 +0100

    refactor: reverse lists

--- js/main.js
+++ js/main.js
@@ -148,27 +148,6 @@
 }());
 
 /*!
- * Reverse Lists
- * @author Chris Burnell <@iamchrisburnell>
- */
-
-
-(function() {
-
-    'use strict';
-
-    var reversedLists = document.querySelectorAll('ol[reversed]'),
-        counterResetValue;
-    if (reversedLists !== null) {
-        for (var i = 0, listLength = reversedLists.length; i < listLength; i++) {
-            counterResetValue = reversedLists[i].getElementsByTagName('li').length + 1;
-            reversedLists[i].style.counterReset = 'item ' + counterResetValue;
-        }
-    }
-
-}());
-
-/*!
  * A simple JSON search
  * @author Mat Hayward - Erskine Design (Original Author)
  * @author Chris Burnell (Slight, poor modifications) <@iamchrisburnell>
