9dc661055c1623bf8b9e0d1d23f22bc7695b4f3d
Chris Burnell <me@chrisburnell.com>
2016-04-15 10:55:59 +0100

    fix: title and lede font weight, author age -> nationality, loadCSS pattern,

--- js/main.js
+++ js/main.js
@@ -453,6 +453,50 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-10353655-1', 'auto');
 ga('send', 'pageview');
 
+/*! CSS rel=preload polyfill. Depends on loadCSS function. [c]2016 @scottjehl, Filament Group, Inc. Licensed MIT    */
+(function( w ){
+    // rel=preload support test
+    if( !w.loadCSS ){
+        return;
+    }
+    var rp = loadCSS.relpreload = {};
+    rp.support = function(){
+        try {
+            return w.document.createElement( "link" ).relList.supports( "preload" );
+        } catch (e) {
+            return false;
+        }
+    };
+
+    // loop preload links and fetch using loadCSS
+    rp.poly = function(){
+        var links = w.document.getElementsByTagName( "link" );
+        for( var i = 0; i < links.length; i++ ){
+            var link = links[ i ];
+            if( link.rel === "preload" && link.getAttribute( "as" ) === "style" ){
+                w.loadCSS( link.href, link );
+                link.rel = null;
+            }
+        }
+    };
+
+    // if link[rel=preload] is not supported, we must fetch the CSS manually using loadCSS
+    if( !rp.support() ){
+        rp.poly();
+        var run = w.setInterval( rp.poly, 300 );
+        if( w.addEventListener ){
+            w.addEventListener( "load", function(){
+                w.clearInterval( run );
+            } );
+        }
+        if( w.attachEvent ){
+            w.attachEvent( "onload", function(){
+                w.clearInterval( run );
+            } )
+        }
+    }
+}( this ));
+
 /*! picturefill - v3.0.2 - 2016-02-12
  * https://scottjehl.github.io/picturefill/
  * Copyright (c) 2016 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT
