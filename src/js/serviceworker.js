2db4166aad2e4d9c8bd59275ea1c8f43c2edf702
Chris Burnell <me@chrisburnell.com>
2016-07-14 16:41:30 +0100

    I think this works

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -4,103 +4,140 @@
  */
 
 
-(function () {
+/* global self, caches, fetch, URL, Response */
+'use strict';
 
-  'use strict';
+var config = {
+  version: '20160714-15',
+  staticCacheItems: [
+    '/css/main.min.css',
+    '/js/main.min.js',
+    '/favicon.png',
+    '/images/avatar.png',
+    '/search.json',
+    '/offline/',
+    '/',
+    '/search/'
+  ],
+  cachePathPattern: /^\/(?:(20[0-9]{2}|about|search|css|images|js)\/(.+)?)?$/,
+  offlineImage: '<svg role="img" aria-labelledby="offline-title"'
+    + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
+    + '<title id="offline-title">Offline</title>'
+    + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
+    + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
+    + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
+  offlinePage: '/offline/'
+};
 
+function cacheName (key, opts) {
+  return `${opts.version}-${key}`;
+}
 
-  // Version
-  var version = '20160714-13';
+function addToCache (cacheKey, request, response) {
+  if (response.ok) {
+    var copy = response.clone();
+    caches.open(cacheKey).then( cache => {
+      cache.put(request, copy);
+    });
+  }
+  return response;
+}
 
-
-  // Cache name definitions
-  var cacheNameStatic = 'static';
-  var currentCacheNames = [
-    cacheNameStatic
-  ];
-
-
-  // A new ServiceWorker has been registered
-  self.addEventListener('install', function (event) {
-    event.waitUntil(
-      caches.open(cacheNameStatic)
-        .then(function (cache) {
-          return cache.addAll([
-            '/',
-            '/about',
-            '/articles',
-            '/links',
-            '/pens',
-            '/search',
-            '/styleguide',
-            '/tags',
-            '/offline',
-            '/css/main.min.css',
-            '/js/main.min.js',
-            'images/avatar.png',
-            'favicon.png',
-            'search.json'
-          ]);
-        })
-    );
+function fetchFromCache (event) {
+  return caches.match(event.request).then(response => {
+    if (!response) {
+      throw Error(`${event.request.url} not found in cache`);
+    }
+    return response;
   });
+}
 
-
-  // A new ServiceWorker is now active
-  self.addEventListener('activate', function (event) {
-    event.waitUntil(
-      caches.keys()
-        .then(function (cacheNames) {
-          return Promise.all(
-            cacheNames.map(function (cacheName) {
-              if (currentCacheNames.indexOf(cacheName) === -1) {
-                return caches.delete(cacheName);
-              }
-            })
-          );
-        })
+function offlineResponse (resourceType, opts) {
+  if (resourceType === 'image') {
+    return new Response(opts.offlineImage,
+      { headers: { 'Content-Type': 'image/svg+xml' } }
     );
-  });
+  } else if (resourceType === 'content') {
+    return caches.match(opts.offlinePage);
+  }
+  return undefined;
+}
 
+self.addEventListener('install', event => {
+  function onInstall (event, opts) {
+    var cacheKey = cacheName('static', opts);
+    return caches.open(cacheKey)
+      .then(cache => cache.addAll(opts.staticCacheItems));
+  }
 
-  // The page has made a request
-  self.addEventListener('fetch', function (event) {
-    var requestURL = new URL(event.request.url);
+  event.waitUntil(
+    onInstall(event, config).then( () => self.skipWaiting() )
+  );
+});
 
-    event.respondWith(
-      caches.match(event.request)
-        .then(function (response) {
+self.addEventListener('activate', event => {
+  function onActivate (event, opts) {
+    return caches.keys()
+      .then(cacheKeys => {
+        var oldCacheKeys = cacheKeys.filter(key => key.indexOf(opts.version) !== 0);
+        var deletePromises = oldCacheKeys.map(oldKey => caches.delete(oldKey));
+        return Promise.all(deletePromises);
+      });
+  }
 
-          if (response) {
-            return response;
-          }
+  event.waitUntil(
+    onActivate(event, config)
+      .then( () => self.clients.claim() )
+  );
+});
 
-          var fetchRequest = event.request.clone();
+self.addEventListener('fetch', event => {
 
-          return fetch(fetchRequest).then(
-            function (response) {
+  function shouldHandleFetch (event, opts) {
+    var request            = event.request;
+    var url                = new URL(request.url);
+    var criteria           = {
+      matchesPathPattern: opts.cachePathPattern.test(url.pathname),
+      isGETRequest      : request.method === 'GET',
+      isFromMyOrigin    : url.origin === self.location.origin
+    };
+    var failingCriteria    = Object.keys(criteria)
+      .filter(criteriaKey => !criteria[criteriaKey]);
+    return !failingCriteria.length;
+  }
 
-              var shouldCache = false;
+  function onFetch (event, opts) {
+    var request = event.request;
+    var acceptHeader = request.headers.get('Accept');
+    var resourceType = 'static';
+    var cacheKey;
 
-              if (response.type === 'basic' && response.status === 200) {
-                shouldCache = cacheNameStatic;
-              }
+    if (acceptHeader.indexOf('text/html') !== -1) {
+      resourceType = 'content';
+    } else if (acceptHeader.indexOf('image') !== -1) {
+      resourceType = 'image';
+    }
 
-              if (shouldCache) {
-                var responseToCache = response.clone();
+    cacheKey = cacheName(resourceType, opts);
 
-                caches.open(shouldCache)
-                  .then(function (cache) {
-                    var cacheRequest = event.request.clone();
-                    cache.put(cacheRequest, responseToCache);
-                  });
-              }
+    if (resourceType === 'content') {
+      event.respondWith(
+        fetch(request)
+          .then(response => addToCache(cacheKey, request, response))
+          .catch(() => fetchFromCache(event))
+          .catch(() => offlineResponse(resourceType, opts))
+      );
+    } else {
+      event.respondWith(
+        fetchFromCache(event)
+          .catch(() => fetch(request))
+            .then(response => addToCache(cacheKey, request, response))
+          .catch(() => offlineResponse(resourceType, opts))
+      );
+    }
+  }
+  if (shouldHandleFetch(event, config)) {
+    onFetch(event, config);
+  }
 
-              return response;
-            }
-          );
-        })
-    );
-  });
-
-})();
+});
