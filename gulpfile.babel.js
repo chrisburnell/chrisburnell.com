cc3fac1d61e31e6de274bd2712e902769df18af2
Chris Burnell <me@chrisburnell.com>
2018-03-14 10:03:25 +0000

    fix JS, again

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -128,7 +128,8 @@ gulp.task('js-lint', () => {
 
 // Concatenate JavaScript
 gulp.task('js-concat', ['js-lint'], () => {
-    return gulp.src([`${paths.js.src}/**/*.js`,
+    return gulp.src([`${paths.js.src}/helpers.js`, // this must be first!
+                      `${paths.js.src}/**/*.js`,
                      `!${paths.js.src}/serviceworker.js`,
                      `!${paths.js.src}/vendors/{loadcss,loadcss-preload-polyfill,svg4everybody}.js`])
         .pipe(plumber())
