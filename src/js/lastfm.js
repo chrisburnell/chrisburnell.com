fcc6b39b5f148b464d008cc3c4a0334e0e894de9
Chris Burnell <me@chrisburnell.com>
2018-10-23 16:45:48 +0100

    add microformats to scrobbled tune

--- src/js/lastfm.js
+++ src/js/lastfm.js
@@ -7,13 +7,15 @@
     "use strict";
 
     const LASTFM_URL = "https://micropub.chrisburnell.com/lastfm.php";
-    const LASTFM_SECTION = document.querySelectorAll(".js-lastfm");
-    const LASTFM_STATUS = document.querySelector(".js-lastfm-status");
+    const LASTFM_ELEMENTS = document.querySelectorAll(".js-lastfm");
+    const LASTFM_LABEL = document.querySelector(".js-lastfm-label");
     const LASTFM_ARTIST = document.querySelector(".js-lastfm-artist");
     const LASTFM_TRACK = document.querySelector(".js-lastfm-track");
     const LASTFM_LINK = document.querySelector(".js-lastfm-link");
+    const LASTFM_STATUS = document.querySelector(".js-lastfm-status");
+    const LASTFM_DATETIME = document.querySelector(".js-lastfm-datetime");
 
-    if (LASTFM_SECTION.length > 0) {
+    if (LASTFM_ELEMENTS.length > 0) {
         fetch(LASTFM_URL)
             .then(helpers.getFetchResponse)
             .then(response => response.json())
@@ -21,15 +23,22 @@
                 // Success!
                 let datetime = new Date();
                 let playingLabel = "🎶 Now Playing";
+                let status = 'playing';
                 if (data.hasOwnProperty("date")) {
                     datetime.setSeconds(Number(data.date.uts));
                     playingLabel = "🎵 Last Played";
+                    status = 'finished';
                 }
-                LASTFM_STATUS.innerHTML = `<time datetime="${datetime.toISOString()}">${playingLabel}</time>`;
-                LASTFM_ARTIST.innerHTML = data["artist"]["#text"] ? ` <em>by</em> ${data["artist"]["#text"]}` : "";
-                LASTFM_TRACK.innerHTML = data["name"] ? ${data["name"] : "";
+                datetime = datetime.toISOString();
+                LASTFM_LABEL.innerHTML = `<time datetime="${datetime}">${playingLabel}</time>`;
+                LASTFM_ARTIST.innerHTML = data["artist"]["#text"] ? data["artist"]["#text"] : "";
+                LASTFM_TRACK.innerHTML = data["name"] ? data["name"] : "";
                 LASTFM_LINK.href = data.url;
-                for (let section of LASTFM_SECTION) {
+                LASTFM_STATUS.value = status;
+                LASTFM_STATUS.innerHTML = status.charAt(0).toUpperCase() + status.slice(1);
+                LASTFM_DATETIME.setAttribute("datetime", datetime);
+                LASTFM_DATETIME.innerHTML = datetime;
+                for (let section of LASTFM_ELEMENTS) {
                     section.removeAttribute("hidden");
                 }
             })
