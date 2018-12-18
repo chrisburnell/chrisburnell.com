3c942c77fff582928450eedab71ff10fdfa2c75b
Chris Burnell <me@chrisburnell.com>
2018-12-18 16:36:33 +0000

    add likes; style music & like pages

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -11,7 +11,7 @@ sitemap:
 
 'use strict';
 
-const VERSION = 'v2.0.45';
+const VERSION = 'v2.0.46';
 // Set up the caches
 const STATIC_CACHE = 'static::' + VERSION;
 const ASSETS_CACHE = 'assets';
