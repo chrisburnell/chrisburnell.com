c98fdad7edde614c1ce1c9f45a482d54a5a2f494
Chris Burnell <me@chrisburnell.com>
2019-01-13 03:15:30 +0000

    simplify URLs for collections

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -11,7 +11,7 @@ sitemap:
 
 'use strict';
 
-const VERSION = 'v2.0.55';
+const VERSION = 'v2.0.56';
 // Set up the caches
 const STATIC_CACHE = 'static::' + VERSION;
 const ASSETS_CACHE = 'assets';
