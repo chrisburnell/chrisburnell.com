6b53aa1e0cf6cfc20c1091189882e4efb1467a0a
Chris Burnell <me@chrisburnell.com>
2019-01-14 17:47:01 +0000

    make all meta text 1rem

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -11,7 +11,7 @@ sitemap:
 
 'use strict';
 
-const VERSION = 'v2.0.57';
+const VERSION = 'v2.0.58';
 // Set up the caches
 const STATIC_CACHE = 'static::' + VERSION;
 const ASSETS_CACHE = 'assets';
