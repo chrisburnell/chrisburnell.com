56cc402e66e7183ce4bfe8837f3c48f31e45c455
Chris Burnell <me@chrisburnell.com>
2019-03-05 15:56:07 +0100

    fix: broken %content-elements

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -11,7 +11,7 @@ sitemap:
 
 'use strict';
 
-const VERSION = 'v2.0.66';
+const VERSION = 'v2.0.67';
 // Set up the caches
 const STATIC_CACHE = 'static::' + VERSION;
 const ASSETS_CACHE = 'assets';
