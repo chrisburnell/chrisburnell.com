2739379848d34d7e896cf6dd1521713ee18e1e19
Chris Burnell <me@chrisburnell.com>
2020-09-27 21:14:05 +0100

    :art: minor rename

--- src/js/sparklines.js
+++ src/js/sparklines.js
@@ -5,16 +5,16 @@
 // Licensed under a CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
 // http://creativecommons.org/publicdomain/zero/1.0/
 ///
-let sparkline = (id, data, width = 160, height = 21, endpoint = true, color = "hsla(0, 0%, 31%, 1)", endpointColor = "hsla(357, 83%, 55%, 0.5)") => {
+let sparkline = (id, values, width = 160, height = 21, endpoint = true, color = "hsla(0, 0%, 31%, 1)", endpointColor = "hsla(357, 83%, 55%, 0.5)") => {
     if (window.HTMLCanvasElement) {
         var c = document.getElementById(id),
             ctx = c.getContext("2d"),
-            total = data.length,
-            max = Math.max.apply(Math, data),
+            total = values.length,
+            max = Math.max.apply(Math, values),
             xStep = (width - 2) / (total - 1),
             yStep = max / height,
             x = 0,
-            y = height - data[0] / yStep + 2;
+            y = height - values[0] / yStep + 2;
         if (window.devicePixelRatio) {
             c.width = 160 * window.devicePixelRatio;
             c.height = 24 * window.devicePixelRatio;
@@ -25,10 +25,10 @@ let sparkline = (id, data, width = 160, height = 21, endpoint = true, color = "h
         ctx.strokeStyle = color;
         ctx.lineWidth = 2;
         ctx.moveTo(x, y);
-        for (let i in data) {
+        for (let i in values) {
             if (i != 0) {
                 x = x + xStep;
-                y = height - data[i] / yStep + 2;
+                y = height - values[i] / yStep + 2;
                 ctx.lineTo(x, y);
             }
         }
