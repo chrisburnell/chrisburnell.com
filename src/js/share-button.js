5d3da825fe8e1847c9ade3c0b3da2f47b0139420
Chris Burnell <me@chrisburnell.com>
2020-10-21 18:09:51 +0100

    :sparkles: Share button in footer

---
+++ src/js/share-button.js
@@ -0,0 +1,20 @@
+(function (win, doc) {
+    const testButton = doc.createElement('button');
+    testButton.setAttribute('type','share');
+    if (testButton.type != 'share') {
+        win.addEventListener('click', function(ev) {
+            ev = ev || win.event;
+            let target = ev.target;
+            let button = target.closest('button[type="share"]');
+            if (button) {
+                const title = doc.querySelector('title').innerText;
+                const url = win.location.href;
+                if (navigator.share) {
+                    navigator.share({ title: title, url: url });
+                } else {
+                    win.location.href='mailto:?subject='+title+'&body='+url;
+                }
+            }
+        });
+    }
+}(this, this.document));
