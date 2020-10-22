583b2b0ce45685f3aac2a1323375aba4788b5255
Chris Burnell <me@chrisburnell.com>
2020-10-22 20:33:31 +0100

    :wastebasket: tidy and eleventy 0.11.1

--- src/js/get-youtube-feed.js
+++ src/js/get-youtube-feed.js
@@ -4,6 +4,14 @@
     let form = document.querySelector(".get-youtube-feed");
     let input = form.querySelector('input');
 
+    let convertURL = text => {
+        if (text.includes("/channel/")) {
+            let channel = text.split("/channel/")[1].replace(/\/$/, "");
+            input.value = `https://www.youtube.com/feeds/videos.xml?channel_id=${channel}`;
+            input.focus();
+        }
+    };
+
     form.addEventListener('submit', event => {
         event.preventDefault();
         convertURL(input.value);
@@ -13,12 +21,4 @@
         event.preventDefault();
         convertURL(input.value);
     });
-
-    let convertURL = text => {
-        if (text.includes("/channel/")) {
-            let channel = text.split("/channel/")[1].replace(/\/$/, "");
-            input.value = `https://www.youtube.com/feeds/videos.xml?channel_id=${channel}`;
-            input.focus();
-        }
-    };
 })();
