a01843b098ac26a176d6f69a2be0b2b1c8ae0acb
Chris Burnell <me@chrisburnell.com>
2021-06-11 11:13:31 +0100

    reorder and button colour update

--- src/eleventy/filters/dates.js
+++ src/eleventy/filters/dates.js
@@ -1,32 +1,12 @@
 const { DateTime } = require("luxon")
 
 module.exports = {
-    prepareDate: value => {
-        // format() expects Date or Number
-        // YAML dates are already in RFC3339 and it gives us dates in UTC.
-        // They may be a string or an object.
-        // If string, convert to RFC date object first.
-        // If an object, no need to do anything. We can directly format it.
-        switch (typeof value) {
-            case "string":
-                return parseISO(value)
-            case "object":
-            default:
-                return value
-        }
-    },
-    friendlyDate: (value, format = "dd LLLL yyyy") => {
-        return DateTime.fromJSDate(new Date(value)).toFormat(format)
-    },
     rfcDate: (value, showTimezone = true) => {
         let format = "yyyy-MM-dd'T'HH:mm:ss" + (showTimezone ? "ZZZ" : "")
         return DateTime.fromJSDate(new Date(value)).toFormat(format)
     },
-    httpDate: (value) => {
-        return DateTime.fromJSDate(new Date(value)).toHTTP()
-    },
-    epoch: (value) => {
-        return new Date(value).getTime()
+    friendlyDate: (value, format = "dd LLLL yyyy") => {
+        return DateTime.fromJSDate(new Date(value)).toFormat(format)
     },
     friendlyTime: (value, showTimezone = true) => {
         if (showTimezone) {
@@ -36,5 +16,11 @@ module.exports = {
     },
     friendlyTimezone: (value) => {
         return DateTime.fromJSDate(new Date(value)).zoneName
+    },
+    httpDate: (value) => {
+        return DateTime.fromJSDate(new Date(value)).toHTTP()
+    },
+    epoch: (value) => {
+        return new Date(value).getTime()
     }
 }
