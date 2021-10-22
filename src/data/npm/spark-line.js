23b0ea0e65ad6ffc34e2d99b1ba56a9632313257
Chris Burnell <me@chrisburnell.com>
2021-10-22 09:10:28 +0100

    fix: npm stats for spark-line now available

--- src/data/npm/spark-line.js
+++ src/data/npm/spark-line.js
@@ -1,17 +1,11 @@
 const CacheAsset = require("@11ty/eleventy-cache-assets")
 
 module.exports = async function() {
-    return {
-        "downloads": 1,
-        "start": "2021-09-21",
-        "end": "2021-10-20",
-        "package": "@chrisburnell/spark-line"
-    }
-    // let url = "https://api.npmjs.org/downloads/point/last-month/@chrisburnell/spark-line"
-    // let json = await CacheAsset(url, {
-    //     duration: "1w",
-    //     type: "json",
-    // })
+    let url = "https://api.npmjs.org/downloads/point/last-month/@chrisburnell/spark-line"
+    let json = await CacheAsset(url, {
+        duration: "1w",
+        type: "json",
+    })
 
-    // return json
+    return json
 }
