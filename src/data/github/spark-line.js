37525cff81b6d386a6ef40fadabaab6736679ecc
Chris Burnell <me@chrisburnell.com>
2021-11-01 13:09:15 +0000

    force re-cache

--- src/data/github/spark-line.js
+++ src/data/github/spark-line.js
@@ -3,7 +3,7 @@ const CacheAsset = require("@11ty/eleventy-cache-assets")
 module.exports = async function() {
     let url = "https://api.github.com/repos/chrisburnell/spark-line"
     let json = await CacheAsset(url, {
-        duration: "1w",
+        duration: "1d",
         type: "json",
     })
 
