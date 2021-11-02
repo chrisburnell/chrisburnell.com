f0c2ef70c4eb46e375a741803e4aceb7bda21b3d
Chris Burnell <me@chrisburnell.com>
2021-11-02 08:43:41 +0000

    don't know what to call this

--- src/data/github/spark-line.js
+++ src/data/github/spark-line.js
@@ -3,7 +3,7 @@ const CacheAsset = require("@11ty/eleventy-cache-assets")
 module.exports = async function() {
     let url = "https://api.github.com/repos/chrisburnell/spark-line"
     let json = await CacheAsset(url, {
-        duration: "1d",
+        duration: "1w",
         type: "json",
     })
 
--- src/data/npm/spark-line.js
+++ src/data/npm/spark-line.js
@@ -3,7 +3,7 @@ const CacheAsset = require("@11ty/eleventy-cache-assets")
 module.exports = async function() {
     let url = "https://api.npmjs.org/downloads/point/last-month/@chrisburnell/spark-line"
     let json = await CacheAsset(url, {
-        duration: "1d",
+        duration: "1w",
         type: "json",
     })
 
