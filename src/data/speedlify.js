0a56ef7c81e5a894749ea011fa7c1e3f8df06c35
Chris Burnell <me@chrisburnell.com>
2021-11-26 10:18:04 +0000

    force refresh speedlify hash

--- src/data/speedlify.js
+++ src/data/speedlify.js
@@ -5,7 +5,7 @@ const author = require("./author.json")
 module.exports = async function() {
     let url = `${author.urls.speedlify}/api/urls.json`
     let json = await CacheAsset(url, {
-        duration: "1w",
+        duration: "1h",
         type: "json",
     })
     return json
