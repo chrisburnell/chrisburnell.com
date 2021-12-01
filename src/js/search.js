e1bcac1c13a174e72cdfc2898865fb526067c19d
Chris Burnell <me@chrisburnell.com>
2021-12-01 10:32:34 +0000

    fix: search page input prepend

--- src/js/search.js
+++ src/js/search.js
@@ -1,11 +1,14 @@
 (() => {
 	"use strict";
 
-	const input = document.querySelector("input");
+	const form = document.getElementById("search");
+	const input = form.querySelector("input");
 
 	input.value = "";
 
-	function prepend() {
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
 		input.value = "site:chrisburnell.com " + input.value;
-	}
+		form.submit();
+	});
 })();
