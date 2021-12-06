696dbaeec504c740bc34d6576d4ebb99a74de101
Chris Burnell <me@chrisburnell.com>
2021-12-06 08:50:14 +0000

    npm cache exists now

--- src/data/npm/eleventy-cache-webmentions.js
+++ src/data/npm/eleventy-cache-webmentions.js
@@ -1,18 +1,11 @@
 const Cache = require("@11ty/eleventy-cache-assets")
 
 module.exports = async () => {
-	// let url = "https://api.npmjs.org/downloads/point/last-month/@chrisburnell/eleventy-cache-webmentions"
-	// let json = await Cache(url, {
-	// 	duration: "1w",
-	// 	type: "json",
-	// })
+	let url = "https://api.npmjs.org/downloads/point/last-month/@chrisburnell/eleventy-cache-webmentions"
+	let json = await Cache(url, {
+		duration: "1w",
+		type: "json",
+	})
 
-	// return json
-
-	return {
-		downloads: 0,
-		start: "2021-11-05",
-		end: "2021-12-04",
-		package: "@chrisburnell/eleventy-cache-webmentions",
-	}
+	return json
 }
