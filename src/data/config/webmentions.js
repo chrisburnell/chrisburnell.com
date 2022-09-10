6ca2b0b18b4bc8476f1b373e22b53222deb54cc8
Chris Burnell <me@chrisburnell.com>
2022-09-10 17:13:27 +0100

    fix way too long WMs

--- src/data/config/webmentions.js
+++ src/data/config/webmentions.js
@@ -1,6 +1,8 @@
 const site = require("#data/site")
 const urlReplacements = require("#data/urlReplacements")
 
+const sanitizeHTML = require("sanitize-html")
+
 // Load .env variables with dotenv
 require("dotenv").config()
 
@@ -14,4 +16,12 @@ module.exports = {
 	urlReplacements: urlReplacements,
 	maximumHtmlLength: 1000,
 	maximumHtmlText: "Mentioned this:",
+	allowedHTML: {
+		allowedTags: ["b", "i", "em", "strong", "a", "code", "s", "ins", "del", "mark", "samp", "var"],
+		allowedAttributes: {
+			a: ["href"],
+		},
+		allowedSchemes: ["http", "https"],
+		nonTextTags: ["style", "script", "textarea", "option", "noscript", "pre"],
+	},
 }
--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,8 +1,8 @@
 const configWebmentions = require("../data/config/webmentions.js")
-
 const dateFilters = require("#filters/dates")
 
 const Webmentions = require("@chrisburnell/eleventy-cache-webmentions")(null, configWebmentions)
+const sanitizeHTML = require("sanitize-html")
 
 module.exports = async () => {
 	const webmentionsByUrl = await Webmentions()
@@ -17,12 +17,19 @@ module.exports = async () => {
 			webmentions: (data) => {
 				const webmentionsForUrl = webmentionsByUrl[configWebmentions.domain + data.page.url] || []
 
-				if (webmentionsForUrl.length) {
-					return webmentionsForUrl.sort((a, b) => {
+				return webmentionsForUrl
+					.map((entry) => {
+						if (entry.data?.content) {
+							entry.data.content = sanitizeHTML(entry.data.content, configWebmentions.allowedHTML)
+							if (entry.data.content.length > configWebmentions.maximumHtmlLength) {
+								entry.data.content = `${configWebmentions.maximumHtmlText} <a href="${entry.data.url}">${entry.data.url}</a>`
+							}
+						}
+						return entry
+					})
+					.sort((a, b) => {
 						return dateFilters.epoch(a.data.published || a.verified_date) - dateFilters.epoch(b.data.published || b.verified_date)
 					})
-				}
-				return []
 			},
 		},
 	}
