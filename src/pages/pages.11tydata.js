d555d5155cfe43c11a9c9fcc3e4e01526e786133
Chris Burnell <me@chrisburnell.com>
2022-09-11 22:11:00 +0100

    really simple now!

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -1,16 +1,14 @@
 const configWebmentions = require("../data/config/webmentions.js")
 
-const Webmentions = require("@chrisburnell/eleventy-cache-webmentions")(null, configWebmentions)
+const { getWebmentions } = require("@chrisburnell/eleventy-cache-webmentions")(null, configWebmentions)
 
-module.exports = async () => {
-	return {
-		layout: "page",
-		tags: ["page"],
-		permalink: "/{{ page.fileSlug }}/index.html",
-		eleventyComputed: {
-			webmentions: async (data) => {
-				return await Webmentions.getWebmentions(configWebmentions, configWebmentions.domain + data.page.url)
-			},
+module.exports = {
+	layout: "page",
+	tags: ["page"],
+	permalink: "/{{ page.fileSlug }}/index.html",
+	eleventyComputed: {
+		webmentions: (data) => {
+			return getWebmentions(configWebmentions, configWebmentions.domain + data.page.url)
 		},
-	}
+	},
 }
--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,18 +1,16 @@
 const configWebmentions = require("../data/config/webmentions.js")
 
-const Webmentions = require("@chrisburnell/eleventy-cache-webmentions")(null, configWebmentions)
+const { getWebmentions } = require("@chrisburnell/eleventy-cache-webmentions")(null, configWebmentions)
 
-module.exports = async () => {
-	return {
-		layout: "post",
-		tags: ["post"],
-		list: "deck",
-		mf_root: "entry",
-		show_webmentions: true,
-		eleventyComputed: {
-			webmentions: async (data) => {
-				return await Webmentions.getWebmentions(configWebmentions, configWebmentions.domain + data.page.url)
-			},
+module.exports = {
+	layout: "post",
+	tags: ["post"],
+	list: "deck",
+	mf_root: "entry",
+	show_webmentions: true,
+	eleventyComputed: {
+		webmentions: (data) => {
+			return getWebmentions(configWebmentions, configWebmentions.domain + data.page.url)
 		},
-	}
+	},
 }
