1c3f8419dc9a3c18565446018423cc34423a4621
Chris Burnell <me@chrisburnell.com>
2023-03-04 16:23:25 +0000

    refactor: add details around TOC

--- src/eleventy/transforms/parse.js
+++ src/eleventy/transforms/parse.js
@@ -17,7 +17,14 @@ module.exports = async (value, outputPath) => {
 		// the permalink.
 		const tocHeadings = $(".generate-toc h2")
 		if (tocHeadings.length) {
-			let tocHtml = `<aside class=" [ meta ] [ flow ] " role="complementary"><div class=" [ box ] [ flow ] [ table-of-contents ] "><ol>`
+			let tocHtml = `
+				<aside class=" [ meta ] " role="complementary">
+					<is-land class=" [ table-of-contents ] " on:visible>
+						<details-utils force-close="(max-width: 850px)" force-restore>
+							<details open>
+								<summary>Table of Contents</summary>
+								<div class=" [ box ] ">
+									<ol>`
 			tocHeadings.each((i, element) => {
 				const headingText = $(element)
 					.html()
@@ -25,7 +32,15 @@ module.exports = async (value, outputPath) => {
 				const headingID = $(element).attr("id") || slugify(headingText.toLowerCase())
 				tocHtml += `<li><a href="#${headingID}">${headingText}</a></li>`
 			})
-			tocHtml += `</ol></div></aside>`
+			tocHtml += `			</ol>
+								</div>
+							</details>
+						</details-utils>
+						<template data-island="once">
+							<script src="/js/components/details-utils.js"></script>
+						</template>
+					</is-land>
+				</aside>`
 			$(tocHtml).insertBefore(".content")
 			$(".generate-toc").removeClass("generate-toc")
 		}
