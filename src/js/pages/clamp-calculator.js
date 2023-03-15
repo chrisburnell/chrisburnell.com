bd5032050bde4be2b7daf84a1d8af3e6fdec31b0
Chris Burnell <me@chrisburnell.com>
2023-03-15 14:07:15 +0000

    use number input for clamp-calculator to allow decimals (up to 3 places)

--- src/js/pages/clamp-calculator.js
+++ src/js/pages/clamp-calculator.js
@@ -56,11 +56,11 @@ A + B = ${preferredSizePixels}px + ${variableSize}vw
 
 Minimum Size = ${preferredSizePixels}px + (${this.limitDecimals(change, 5)} * ${this.inputs["viewport-min"].value}px)
 Minimum Size = ${preferredSizePixels}px + ${this.limitDecimals(change, 5) * this.inputs["viewport-min"].value}px
-Minimum Size = ${this.inputs["size-min"].value}px
+Minimum Size = ${this.inputs["size-min"].value}px ✅
 
 Maximum Size = ${preferredSizePixels}px + (${this.limitDecimals(change, 5)} * ${this.inputs["viewport-max"].value}px)
 Maximum Size = ${preferredSizePixels}px + ${this.limitDecimals(change, 5) * this.inputs["viewport-max"].value}px
-Maximum Size = ${this.inputs["size-max"].value}px`
+Maximum Size = ${this.inputs["size-max"].value}px ✅`
 
 		this.measureViewport()
 	}
