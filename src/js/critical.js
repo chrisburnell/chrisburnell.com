f5a206cd8ed8bddb686d13587187ee445db2ea00
Chris Burnell <me@chrisburnell.com>
2024-01-05 18:59:22 +0800

    minor colour fixes; theme selector fix

--- src/js/critical.js
+++ src/js/critical.js
@@ -1,5 +1,3 @@
 document.documentElement.classList.remove("no-js")
 
-import "@daviddarnes/storage-form/storage-form"
-
 // import "./imports/sotb-banner-setup"
--- src/js/defer.js
+++ src/js/defer.js
@@ -1,5 +1,8 @@
+import "@daviddarnes/storage-form/storage-form"
+
 import "@11ty/is-land/is-land"
 
+// Not used until SotB is announced
 // import "./imports/sotb-banner"
 
 import "./imports/polyfill-details-exclusive"
