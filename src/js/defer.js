8a11e76a35d91d9b0a26ad8ca2af6333c55e89eb
Chris Burnell <me@chrisburnell.com>
2024-02-25 13:07:29 +0800

    polyfill popover

--- src/js/defer.js
+++ src/js/defer.js
@@ -1,8 +1,10 @@
 import "@daviddarnes/storage-form/storage-form"
 
+import "@oddbird/popover-polyfill"
+
 import "@11ty/is-land/is-land"
 
-import "@chrisburnell/svg-sparkline/svg-sparkline"
+import "@chrisburnell/svg-sparkline"
 
 import "./imports/website-sounds"
 
