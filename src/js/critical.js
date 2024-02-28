6bff66d5d43cf15326dc77dbeb3ad8516feb7b52
Chris Burnell <me@chrisburnell.com>
2024-02-28 15:31:16 +0800

    fix: only remove data-theme when DOM is loaded

--- src/js/critical.js
+++ src/js/critical.js
@@ -5,7 +5,8 @@ const THEME = localStorage.getItem(STORAGE_KEY)
 
 if (THEME) {
 	document.documentElement.setAttribute(`data-${STORAGE_KEY}`, THEME)
-	setTimeout(() => {
+
+	window.addEventListener("load", function () {
 		document.documentElement.removeAttribute(`data-${STORAGE_KEY}`)
-	}, 1000)
+	})
 }
