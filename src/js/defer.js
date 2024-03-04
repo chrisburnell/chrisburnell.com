c1f6c0880dc847f042a4b84ff70f5db84b40edc0
Chris Burnell <me@chrisburnell.com>
2024-03-05 02:27:53 +0800

    add fathom analytics

--- src/js/defer.js
+++ src/js/defer.js
@@ -1,11 +1,11 @@
-import "@daviddarnes/storage-form/storage-form"
-
-import "@oddbird/popover-polyfill"
-
-import "@11ty/is-land/is-land"
-
-import "@zachleat/webcare-webshare/webcare-webshare"
-
-import "./imports/website-sounds"
-
-import "./imports/welcome"
+(function(f, a, t, h, o, m){
+	a[h]=a[h]||function(){
+		(a[h].q=a[h].q||[]).push(arguments)
+	};
+	o=f.createElement('script'),
+	m=f.getElementsByTagName('script')[0];
+	o.async=1; o.src=t; o.id='fathom-script';
+	m.parentNode.insertBefore(o,m)
+})(document, window, '//logs.chrisburnell.com/tracker.js', 'fathom');
+fathom('set', 'siteId', 'NMGWH');
+fathom('trackPageview');
