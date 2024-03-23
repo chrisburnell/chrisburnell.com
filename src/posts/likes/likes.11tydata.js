80cf0d5c1a94b341eb17f9c7c84f1b35108e37ad
Chris Burnell <me@chrisburnell.com>
2024-03-23 14:17:40 +0800

    🤦‍♂️ was missing u-like-of for ALL LIKES FOR HOW LONG??!

--- src/posts/likes/likes.11tydata.js
+++ src/posts/likes/likes.11tydata.js
@@ -3,6 +3,7 @@ import emojis from "../../eleventy/data/emojis.js"
 export default {
 	permalink: "/like/{{ page.fileSlug }}/index.html",
 	list: "deck",
+	mf_property: "like-of",
 	category: "like",
 	categoryPlural: "likes",
 	categoryCode: "f",
