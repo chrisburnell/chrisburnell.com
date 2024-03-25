ea56b5a7a993bf60a337ff8b90c73020a3334faf
Chris Burnell <me@chrisburnell.com>
2024-03-26 01:26:53 +0800

    fix: critically mis-implemented storage-form

--- src/js/critical.js
+++ src/js/critical.js
@@ -1,12 +1,11 @@
 document.documentElement.classList.remove("no-js")
 
-const STORAGE_KEY = "theme"
-const THEME = localStorage.getItem(STORAGE_KEY)
+const THEME = localStorage.getItem("theme")
 
 if (THEME) {
-	document.documentElement.setAttribute(`data-${STORAGE_KEY}`, THEME)
+	document.documentElement.setAttribute("data-theme", THEME)
 
 	window.addEventListener("load", function () {
-		document.documentElement.removeAttribute(`data-${STORAGE_KEY}`)
+		document.documentElement.removeAttribute("data-theme")
 	})
 }
