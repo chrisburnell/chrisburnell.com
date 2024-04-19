40ca32329f8f7cc252d78c34504e86c15fd72e54
Chris Burnell <me@chrisburnell.com>
2024-04-19 23:45:12 +0800

    make the welcome message in console a little spicier 🌶️

--- src/js/imports/welcome.js
+++ src/js/imports/welcome.js
@@ -7,19 +7,25 @@ class Welcome {
 	 * @constructor
 	 */
 	constructor() {
-		console.log(`%c                        █▒▒      %cchrisburnell.com`, "color: #507791", "color: inherit")
-		console.log(`%c ▒▒▒▒▒▒▒▒▒             ▒███▓     %c--------------------------`, "color: #507791", "color: inherit")
-		console.log(`%c  ▒▓███████▓▓▒       ▒█▓████▓    %cOS: ${navigator.oscpu}`, "color: #507791", "color: inherit")
-		console.log(`%c     ▒██████▓▒▓▒   ▒▓▓██████▓█▓  %cLanguage: ${navigator.language}`, "color: #507791", "color: inherit")
-		console.log(`%c      ▒▓██████▒▓█▒▒█████████▓▒▒  %cOnline: ${navigator.onLine === true ? "True" : "False"}`, "color: #507791", "color: inherit")
-		console.log(`%c         ▓███████████████▓▓▒     %cService Worker: ${navigator?.serviceWorker?.controller?.state || "pending"}`, "color: #507791", "color: inherit")
-		console.log(`%c             ▓███████▓▒▒▓▒`, "color: #507791")
-		console.log(`%c         ▒▒▒▓██▓▓▓▓▒▓▓█▓▒▓`, "color: #507791")
-		console.log(`%c ████████████▓█▓█▓▒▓  ▒▒▓▓`, "color: #507791")
-		console.log(`%c   ▒▓█▓▓▓▒▓██▓▓██▓▒      ▒`, "color: #507791")
-		console.log(`%c         ▒▒   ▒▓█▓▓▒`, "color: #507791")
-		console.log(`%c              ▒▓█  █▓▓`, "color: #507791")
-		console.log(`%c               ▒     ▒`, "color: #507791")
+		const osString = `OS:              ${navigator.oscpu}`
+		const languageString = `Language:        ${navigator.language}`
+		const networkString = `Network:         ${navigator.onLine === true ? "Online" : "Offline"}`
+		const swStatus = (navigator?.serviceWorker?.controller?.state || "pending")
+		const swString = `Service Worker:  ${swStatus.charAt(0).toUpperCase() + swStatus.slice(1)}`
+
+		console.log(`%c                        ░█░█      %cchrisburnell.com`, "color: #507791", "color: inherit")
+		console.log(`%c  ██░░░░░█░             █████     %c${"-".repeat(Math.max(osString.length, languageString.length, networkString.length, swString.length))}`, "color: #507791", "color: inherit")
+		console.log(`%c   ░▓▓██▓███▓█░       ██▒░▓███    %c${osString}`, "color: #507791", "color: inherit")
+		console.log(`%c     ░░█▓▒██▓░░█░   █▓░██▓██▓██░░ %c${languageString}`, "color: #507791", "color: inherit")
+		console.log(`%c      ░█░███▓▓██░█░█▓███░░█▓██░█  %c${networkString}`, "color: #507791", "color: inherit")
+		console.log(`%c        ░░▒██▓░▒█░██▓███▓▓██▓░    %c${swString}`, "color: #507791", "color: inherit")
+		console.log(`%c           ░░▓██████▒█▓▒▓▒░░`, "color: #507791")
+		console.log(`%c             ░███▓▒▓▒░░▓▓░░░`, "color: #507791")
+		console.log(`%c ▒█▒▓▒▓▓▓▓▓██▓▓░░▓░░▓▒░███▒░`, "color: #507791")
+		console.log(`%c  ░▓███▓██░▒▒░░▒██▒▓█     █▒`, "color: #507791")
+		console.log(`%c      ░██▒░░░██░▒▓█░       ░`, "color: #507791")
+		console.log(`%c              ░▒█▓░░▓█░`, "color: #507791")
+		console.log(`%c               ░▒▓░░▒░░░`, "color: #507791")
 		console.log(`Checking out the source code, %ceh%c?`, "color: #e0151f", "color: inherit")
 		console.log(`Get in touch with me if you want to know more! %chttps://chrisburnell.com/about/#contact`, "color: #e0151f")
 	}
