7e5ffa950dcee4d8d0870018faf0267d0c1cb833
Chris Burnell <me@chrisburnell.com>
2024-05-04 03:13:16 +0800

    add PGP key to links page [skip ci]

--- src/eleventy/data/author.js
+++ src/eleventy/data/author.js
@@ -39,6 +39,7 @@ export const nationality = "Canadian"
 export const email = "me@chrisburnell.com"
 export const avatar = "/images/avatar@4x.jpeg"
 export const description = `I’m <span class=" [ canada ] ">${name}</span> <button id="name" class=" [ anchor  small ] [ js-name-button  no-feed ] " title="Hear my name read aloud" tabindex="-1">/<span title="'k' in 'kind'">k</span><span title="'r' in 'rye'">r</span><span title="'i' in 'rabbit'">ɪ</span><span title="'s' in 'sigh'">s</span> <span title="'b' in 'buy'">b</span><span title="/ɜːr/: 'ur' in 'fur'">ɜːr</span><span title="/ˈ/: primary stress follows">ˈ</span><span title="'n' in 'nigh'">n</span><span title="/ɛ/: 'e' in 'dress'">ɛ</span><span title="'l' in 'lie'">l</span>/</button> (<a href="https://en.pronouns.page/@${pronouns_page}">${pronouns}</a>), a <span class=" [ canada ] ">Canadian</span> <span class="p-job-title">${role}</span> / Software Engineer living in <a href="https://www.openstreetmap.org/#map=14/1.3156/103.9425" class="p-locality">Singapore</a>. I’m often found <a href="/projects/">rabbit hole deep-diving</a>. This is where I think, write, and share my passion for front end development and the web.`
+export const pgp_styled = pgp.replace(/(.{4})/g, `<span style="margin-inline-end: 1ex;">$1</span>`)
 
 export const _employer = {
 	title: "",
@@ -50,10 +51,11 @@ export default Object.assign({}, metadata, {
 	firstname,
 	lastname,
 	shortname,
-	description,
 	role,
 	nationality,
 	email,
 	avatar,
+	description,
+	pgp_styled,
 	_employer,
 })
