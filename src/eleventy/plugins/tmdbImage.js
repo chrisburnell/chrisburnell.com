339635abf7309e6bf74e523d11f4ff24ae93d115
Chris Burnell <me@chrisburnell.com>
2024-05-30 17:52:16 +0800

    update funding options [skip ci]

---
+++ src/eleventy/plugins/tmdbImage.js
@@ -0,0 +1,74 @@
+import EleventyFetch from "@11ty/eleventy-fetch"
+import Image from "@11ty/eleventy-img"
+import dotenv from "dotenv"
+dotenv.config()
+
+const getImageOptions = () => {
+	return {
+		width: [400],
+		formats: process.env.ELEVENTY_RUN_MODE === "build" ? ["avif", "webp", "jpg"] : ["webp", "jpg"],
+		urlPath: "/images/covers/",
+		outputDir: "./_site/images/covers",
+		duration: "*",
+		sharpOptions: {
+			quality: 100,
+		},
+	}
+}
+
+const fetchImageData = (options, src) => {
+	if (!src) {
+		throw new Error("src property required in `tmdbImage` shortcode.")
+	}
+
+	Image(src, getImageOptions(options)).then(() => {
+		// return nothing, even though this returns a promise
+	})
+}
+
+const storePoster = async (id) => {
+	// We know where the images will be
+	const fakeUrl = `/images/covers/${id}.jpg`
+	const metadata = Image.statsByDimensionsSync(fakeUrl, size, size, getImageOptions())
+	const markup = Image.generateHTML(
+		metadata,
+		{
+			alt: `TODO`,
+			title: `TODO`,
+			sizes: "100vw",
+			loading: "lazy",
+			decoding: "async",
+		},
+		{
+			whitespaceMode: "inline",
+		},
+	)
+
+	return markup
+}
+
+export default function (eleventyConfig) {
+	let posters
+
+	eleventyConfig.on("beforeBuild", () => {
+		posters = new Set()
+	})
+
+	if (process.env.ELEVENTY_RUN_MODE === "build") {
+		eleventyConfig.on("afterBuild", () => {
+			let array = Array.from(posters)
+			console.log(`[${getHost(siteURL)}] Generating ${array.length} movie/TV posters.`)
+			for (let poster of array) {
+				fetchImageData(poster.url, poster.photo)
+			}
+		})
+	}
+
+	eleventyConfig.addNunjucksAsyncShortcode("tmdbImage", async (id) => {
+		const dataURL = `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.TMDB_API_KEY}`
+		const dataJSON = await EleventyFetch(dataURL, {
+			duration: "*",
+			type: "json",
+		})
+	})
+}
---
+++ src/posts/articles/api-listing.md
@@ -0,0 +1,20 @@
+---
+draft: true
+date: 2024-05-08T02:20:00+0800
+title: The &lt;api-listing&gt; web component
+description: TODO
+tags:
+  - weblogpomo
+  - weblogpomo2024
+  - javascript
+post_includes:
+  - weblogpomo2024.njk
+---
+
+Idea:
+
+Refactor `<lastfm-listening>` into a web component that can consume any API call that returns an array of data
+
+Use David Darnes’ mastodon-post https://github.com/daviddarnes/mastodon-post to create templates to map the data and output desired HTML from the API call
+
+Figure out how it can be used both in Node and front end?
