e001945b2e4898b45b046a555b0452e8727b5916
Chris Burnell <me@chrisburnell.com>
2025-01-12 11:23:58 -0400

    fix: broken drink_of links add: new beer checkins

--- src/eleventy/data/categoryIntroductions.js
+++ src/eleventy/data/categoryIntroductions.js
@@ -3,6 +3,8 @@ export default {
 	beer: [
 		`Connect with me on <a href="https://untappd.com/user/chrisburnell">Untappd</a> or check out my special collections of reviews:`,
 		`<ul class=" [ cluster ] "><li><a href="/pigsear2018/">Pig’s Ear 2018</a></li><li><a href="/pigsear2019/">Pig’s Ear 2019</a></li><li><a href="/pigsear2021/">Pig’s Ear 2021</a></li><li><a href="/advent2021/">Advent Calendar 2021</a></li><li><a href="/pigsear2022/">Pig’s Ear 2022</a></li><li><a href="/pigsear2023/">Pig’s Ear 2023</a></li></ul>`,
+		`I tend to be quite generous in my scoring, so here’s what my beer scores generally mean:`,
+		`<ul><li>0 stars: I wouldn’t take money to drink this, and there might even be permanent physical damage from consumption</li><li>★ 1 star: Truly awful, not even when it’s cold and free on a hot day</li><li>★★ 2 stars: A disappointing performance; Not completely devoid of good but overwhelmed by the bad</li><li>★★★ 3 stars: Past the midway point now, this is getting pretty good; although, might get tiresome after a session</li><li>★★★★ 4 stars: Remarkable and highly recommended; Just a few notches short of perfection</li><li>★★★★★ 5 stars: Exceptional; Can only really be described as perfect</li></ul>`,
 	],
 	code: [
 		`<dl class="full"><dt>What’s a <q>pen</q>?</dt><dd><q><a href="https://codepen.io"><em>CodePen</em></a> is an HTML, CSS, and JavaScript code editor in your browser with instant previews.</q></dd></dl>`,
