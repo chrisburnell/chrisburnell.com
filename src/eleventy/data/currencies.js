82b97f829bee21372634eb7ea7c0968242f48e57
Chris Burnell <me@chrisburnell.com>
2025-02-07 17:28:03 -0400

    refactor: add more currencies and clean up converter

--- src/eleventy/data/currencies.js
+++ src/eleventy/data/currencies.js
@@ -2,11 +2,15 @@ import EleventyFetch from "@11ty/eleventy-fetch";
 import { cacheDurations } from "./site.js";
 
 const currencies = async function () {
-	const url = `https://api.frankfurter.dev/v1/latest?base=EUR&symbols=CAD,DKK,GBP,JPY,MYR,SGD,THB,USD`;
+	const url = `https://api.frankfurter.dev/v1/latest?base=EUR&symbols=AUD,CAD,DKK,GBP,IDR,INR,JPY,MXN,MYR,NZD,SGD,THB,USD`;
 	const json = await EleventyFetch(url, {
 		duration: cacheDurations.daily,
 		type: "json",
 	});
+
+	// Add base currency to rates
+	json.rates[json.base] = json.amount;
+
 	return json;
 };
 
