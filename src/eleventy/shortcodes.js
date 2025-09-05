3f563206d8c0c55bd87dd31fdbb65fe01c28ad9e
Chris Burnell <me@chrisburnell.com>
2025-09-04 22:06:43 -0300

    fun: add ::after content to javascript bookmarklet anchors [skip ci]

--- src/eleventy/shortcodes.js
+++ src/eleventy/shortcodes.js
@@ -15,18 +15,21 @@ const sparkline = (collection, start, animate = false, curve = true) => {
 		values.push(getCollectionCountByYear(collection, year));
 	}
 	// Sparklines in A minor
-	return `<svg-sparkline values="${values.join(",")}"
-						fill="true"
-						start-label="${start}"
-						end-label="${currentYear}"
-						${animate ? `animate="${animate}"` : ""}
-						${curve ? `curve="${curve}"` : ""}
-						class=" [ pentatonic ] "
-						key-start="25"
-						key-intervals="2,1,2,2,1,2,2"
-						key-limit="15"
-						title="Click to hear me!"></svg-sparkline>
-			<is-land class=" [ visually-hidden ] "><template webc:raw data-island="once"><script type="module" src="/js/components/svg-sparkline.js"></script></template></is-land>`;
+	return `
+		<svg-sparkline
+			values="${values.join(",")}"
+			fill="true"
+			start-label="${start}"
+			end-label="${currentYear}"
+			${animate ? `animate="${animate}"` : ""}
+			${curve ? `curve="${curve}"` : ""}
+			class=" [ pentatonic ] "
+			key-start="25"
+			key-intervals="2,1,2,2,1,2,2"
+			key-limit="15"
+			title="Click to hear me!"></svg-sparkline>
+		<is-land class=" [ visually-hidden ] "><template webc:raw data-island="once"><script type="module" src="/js/components/svg-sparkline.js"></script></template></is-land>
+	`.trim();
 };
 
 export default {
